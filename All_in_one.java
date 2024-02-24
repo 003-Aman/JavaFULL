acaa227f8fe5dcbede1b70add233ed2912916246
003-Aman <amanshrestha3003@gmail.com>
2024-02-24 00:28:00 -0500

    this is basics

--- All_in_one.java
+++ All_in_one.java
@@ -1,8 +1,17 @@
 
 
+
+import java.util.Scanner;
 class All_in_one{
     public static void main(String[] args) {
-        System.out.print("Lets start this shit")
+        //Basic print
+        System.out.print("Lets start this shit");
+        //baisc inputs from user of different datatypes
+        Scanner input = new Scanner(System.in);//necessary for input
+        int number = input.nextInt();
+        String sentence = input.nextLine();
+        float decimal = input.nextFloat();
+
         
     }
 }
