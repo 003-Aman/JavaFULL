6bf3ef754ce7ef891d8402f2763ee82c1b294043
003-Aman <amanshrestha3003@gmail.com>
2024-02-24 00:40:03 -0500

    isn't running but should be right

--- All_in_one.java
+++ All_in_one.java
@@ -3,14 +3,27 @@
 
 import java.util.Scanner;
 class All_in_one{
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         //Basic print
-        System.out.print("Lets start this shit");
+        System.out.println("Lets start this shit");
         //baisc inputs from user of different datatypes
         Scanner input = new Scanner(System.in);//necessary for input
         int number = input.nextInt();
         String sentence = input.nextLine();
         float decimal = input.nextFloat();
+        //basic for and while loop
+        int count = 0;
+        for(int i=0; i<=10;i++){
+            count += 10;
+        }
+        System.out.println(count);
+
+        // while and do-while are very similar
+        int i;
+        while(int i<=10){System.out.println(i);i++;}
 
         
     }
