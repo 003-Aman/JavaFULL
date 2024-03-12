6e5b54a68cd66e42bac376174df8bc1b67b1663b
003-Aman <amanshrestha3003@gmail.com>
2024-03-12 00:46:28 -0400

    Lets start a java tutorial

---
+++ FreeCodeCamp.java
@@ -0,0 +1,13 @@
+//public, private define the accessibility of class members, which play a crucial role in object interaction and encapsulation
+
+class Cat{
+    
+     String color;
+     String mood;
+
+
+     void purr(){
+        System.out.println("Cat purrs");
+     }
+}
+
