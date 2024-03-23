284c7dd3b016e89ce89264d9549be36777ff0ced
003-Aman <amanshrestha3003@gmail.com>
2024-03-23 18:56:43 -0400

    patternQ

---
+++ PatternQ.java
@@ -0,0 +1,42 @@
+public class PatternQ {
+    public static void main(String[] args) {
+       int n = 10;
+       int m =10; 
+    
+    //print a rectangle using loops
+    for(int i =1;i<=n;i++){
+        for(int j =1;j<=m;j++){
+            if(i==1||j==1||i==n||j==m){
+                System.out.print("*");}
+            else{
+                System.out.print(" ");
+            }
+
+            }
+        System.out.println();}
+
+    //print a right angled triangle
+    int a = 10;
+    for (int i=1;i<=a;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+
+        
+    }
+    // rotated right angled traingle
+    int b = 10;
+    for(int i=b;i>=1;i--){
+        for(int j=i;j>=1;j--){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    
+    
+
+    
+}
+}
